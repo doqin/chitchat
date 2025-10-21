dfda186a274d36980f517fca527b1e52fe9358d7
Huy Nguyễn Lê Đức <24520680@ms.uit.edu.vn>
2025-10-21 09:41:32 +0700

    Add splash intro

---
+++ Client/SplashScreen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class SplashScreen : Form
+    {
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+
+            timer.Interval = 3000; // 3 giây
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            this.Hide();
+            new ServerDiscoveryForm().Show(); // Form chính của app
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            this.Opacity = 0;
+
+            System.Windows.Forms.Timer fade = new System.Windows.Forms.Timer { Interval = 50 };
+
+            fade.Tick += (s, ev) =>
+            {
+                if (this.Opacity < 1)
+                    this.Opacity += 0.05;
+                else
+                    fade.Stop();
+            };
+            fade.Start();
+        }
+
+    }
+
+}
